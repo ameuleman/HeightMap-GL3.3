fefdf8edf9c95163645eaa2b8e9fc1b77770c701
Andréas Meuleman <andreasmeu@gmail.com>
2017-03-18 19:00:52 +0100

    Comment

--- src/ParallelTool.h
+++ src/ParallelTool.h
@@ -1,9 +1,30 @@
 #ifndef PARALLELTOOL_H
 #define PARALLELTOOL_H
 
+/**
+*******************************************************************************
+*
+*  @file       ParallelTool.h
+*
+*  @brief      Class to handle functions to simplify parallel processing
+*
+*  @author     Andréas Meuleman
+*******************************************************************************
+*/
+
+
+//******************************************************************************
+//  Include
+//******************************************************************************
 #include <thread>
 
 
+//==============================================================================
+/**
+*  @class  ParallelTool
+*  @brief  ParallelTool is Class to lhandle functions to simplify parallel processing
+*/
+//==============================================================================
 class ParallelTool
 {
 public:
@@ -23,6 +44,7 @@ template<class F> void ParallelTool::performInParallel(
 {
     parallelismLvl *= 2;
 
+    //If we can increase parralelism, we split the workload into to threads
     if(parallelismLvl <= maxParallelism)
     {
         unsigned int midIndex((unsigned int)(rightIndex / 2));
@@ -43,6 +65,7 @@ template<class F> void ParallelTool::performInParallel(
     }
     else
     {
+        //Use the sequential function
         functor(leftIndex, rightIndex);
     }
 }
@@ -51,6 +74,7 @@ template<class F> void ParallelTool::performInParallel(
 template<class F> void ParallelTool::performInParallel(F const& functor)
 //------------------------------------------------------------------------------
 {
+        //Launch a thread for the functor
         std::thread parallelProcessing(
             [&functor]()
             {
