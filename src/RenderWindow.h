d05a9017ce5577a06d0d4c108ac3e5482dd1cca4
Andréas Meuleman <andreasmeu@gmail.com>
2017-03-18 19:02:35 +0100

    Comment

--- src/RenderWindow.h
+++ src/RenderWindow.h
@@ -8,9 +8,7 @@
 *
 *  @brief      Class to display the height map
 *
-*  @date       17/06/2016
-*
-*  @author     Andréas Meuleman
+*  @author     AndrÃ©as Meuleman
 *******************************************************************************
 */
 
